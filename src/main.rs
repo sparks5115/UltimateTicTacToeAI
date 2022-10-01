a99295542622c7519952bfecdf3d13713015bc3f
Sidney <segoldinger@wpi.edu>
2022-10-01 00:54:46 -0400

    commenting out disfunctional code but uncomment and make changes at your pace

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ pub fn main() {
 
         // read in move_file
 
-        // determine step
+        // determine step (Moove)
 
         // write to move_file
 
@@ -36,32 +36,32 @@ pub fn main() {
 
     }
 }
-
-fn minimax(maximizingPlayer: bool, depth: i32, mut alpha: i32, mut beta: i32, node: TreeNode) {
+/*
+fn minimax(maximizingPlayer: bool, depth: i32, mut alpha: i32, mut beta: i32, node: TreeNode) -> i32 {
     // if depth == 0 or terminal node
     if (depth == 0) || (0 != 1) {
 
     }
 
     if maximizingPlayer {
-        let bestValue = -inf;
+        let mut bestValue = i32::MIN;
 
         // loop through child nodes
         for child in node.children {
-            let value = minimax (!maximizingPlayer, depth-1, alpha, beta, child);
-            bestValue = max(bestValue, value);
+            let value = minimax(!maximizingPlayer, depth-1, alpha, beta, child);
+            bestValue = i32::max(bestValue, value);
             alpha = max(alpha, bestValue);
             if beta <= alpha { break; }
         }
         return bestValue;
     }
 
-    if !maximizingPlayer {
-        let bestValue = inf;
+    elif !maximizingPlayer {
+        let mut bestValue = i32::MAX;
 
         // loop through child nodes
         for child in node.children {
-            let value = minimax (!maximizingPlayer, depth-1, alpha, beta, child);
+            let value = minimax(!maximizingPlayer, depth-1, alpha, beta, child);
             bestValue = min(bestValue, value);
             if beta <= alpha { break; }
         }
@@ -69,3 +69,4 @@ fn minimax(maximizingPlayer: bool, depth: i32, mut alpha: i32, mut beta: i32, no
     }
 
 }
+*/
